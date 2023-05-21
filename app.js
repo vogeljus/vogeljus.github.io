2b7a1feed91117b36bb109da47ff0e7240d7e09e
vogeljus <134195595+vogeljus@users.noreply.github.com>
2023-05-21 13:49:24 +0200

    include forecast

--- app.js
+++ app.js
@@ -10,12 +10,50 @@ form.addEventListener('submit', async (event) => {
   try {
     const response = await fetch(url);
     const data = await response.json();
-    const { temp_c, humidity, wind_kph } = data.current;
+    const { temp_c, cloud, humidity, wind_kph } = data.current;
     weatherInfo.innerHTML = `
       <p>Temperature: ${temp_c}°C</p>
+      <p>Cloud: ${cloud}</p>
       <p>Humidity: ${humidity}%</p>
       <p>Wind Speed: ${wind_kph} kph</p>
     `;
+  } catch (error) {
+    console.error(error);
+    weatherInfo.innerHTML = '<p>Unable to get weather information</p>';
+  }
+});
+
+
+const form = document.querySelector('form');
+const weatherInfo = document.querySelector('#weather-info');
+
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+  const city = event.target.elements.city.value;
+  const apiKey = 'YOUR_API_KEY_HERE';
+  const url = `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${city}&days=3`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    const forecast = data.forecast.forecastday;
+    let forecastHtml = '';
+
+    forecast.forEach((day) => {
+      const date = day.date;
+      const { maxtemp_c, cloud, mintemp_c, condition } = day.day;
+      forecastHtml += `
+        <div>
+          <h3>${date}</h3>
+          <p>High: ${maxtemp_c}°C</p>
+          <p>Cloud: ${cloud}</p>
+          <p>Low: ${mintemp_c}°C</p>
+          <p>Condition: ${condition.text}</p>
+        </div>
+      `;
+    });
+
+    weatherInfo.innerHTML = forecastHtml;
   } catch (error) {
     console.error(error);
     weatherInfo.innerHTML = '<p>Unable to get weather information</p>';
